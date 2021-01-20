177083d429365df2351b86e5366df4fb3e16b861
dyl25 <dylan-vst@hotmail.com>
2021-01-20 14:32:08 +0100

    delete some files and add react-router

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <nav className="navbar" aria-label="main navigation">
+            <div id="mainNav" className="navbar-menu">
+                <div className="navbar-start">
+                    <a className="navbar-item">Home</a>
+                    <a className="navbar-item">Blogs</a>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
